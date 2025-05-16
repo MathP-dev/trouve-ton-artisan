a9196fcd653b7071837e96ee441350c457cb2790
Mathieu P. <mathieu.paquier85@gmail.com>
2025-05-16 21:23:23 +0200

    adding page not found 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CategoryComponent } from './category/category.component';
 import { ArtisanDetailComponent } from './artisan-detail/artisan-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'artisans/:category', component: CategoryComponent },
-  { path: 'artisan/:id', component: ArtisanDetailComponent }
+  { path: 'artisan/:id', component: ArtisanDetailComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
