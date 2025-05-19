1f1648986f05e08bde9fbd5e3ad94eed241ae04c
Mathieu P. <mathieu.paquier85@gmail.com>
2025-05-19 11:33:23 +0200

    SearchBar configuration, and fix header logo

---
+++ src/app/searchresults/searchresults.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-searchresults',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './searchresults.component.html',
+  styleUrl: './searchresults.component.css'
+})
+export class SearchresultsComponent implements OnInit, OnDestroy {
+  searchResults: any[] = [];
+  private searchSubscription: Subscription | null = null;
+
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit(): void {
+    this.searchSubscription = this.searchService.searchResults$.subscribe(results => {
+      this.searchResults = results;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+  }
+
+}
